096f191ba0fd27411f114adaf7ad176278eb77ad
Louis Opter <louis@opter.org>
2023-11-09 15:30:01 -0800

    Update nixpkgs and alpine docker images

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "An homelab";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
--- third_party/docker/images.nix
+++ third_party/docker/images.nix
@@ -1,18 +1,21 @@
 {
+  # To update images, use docker pull to get the new imageDigest, then empty
+  # out sha256 and run nix build so that it tells you the new value to put
+  # there:
   alpineDetailsBySystem = {
     x86_64-linux = {
       imageName = "amd64/alpine";
-      imageDigest = "sha256:4ff3ca91275773af45cb4b0834e12b7eb47d1c18f770a0b151381cd227f4c253";
-      sha256 = "rBfwij9kDWqq1g+AGdoaQLWmV9Wa3Oxv/H0XYRHT4Xg=";
+      imageDigest = "sha256:48d9183eb12a05c99bcc0bf44a003607b8e941e1d4f41f9ad12bdcc4b5672f86";
+      sha256 = "sha256-XfnogxDJPYwyDQWqz7vyxfJoaam0DSTcwwTvq4aZC/8=";
       finalImageName = "amd64/alpine";
-      finalImageTag = "3.16.0";
+      finalImageTag = "3.18.4";
     };
     armv6l-linux = {
       imageName = "arm32v6/alpine";
-      imageDigest = "sha256:e047bc2af17934d38c5a7fa9f46d443f1de3a7675546402592ef805cfa929f9d";
-      sha256 = "uYg5MFs87WlHwwYdWDWm4bKVzWc5ifmHzrCTw6idQsw=";
+      imageDigest = "sha256:777e2106170c66742ddbe77f703badb7dc94d9a5b1dc2c4a01538fad9aef56bb";
+      sha256 = "sha256-gxcOeft00YleYOw3CZAAeY0nHSqT9PVeJHqH464TPok=";
       finalImageName = "arm32v6/alpine";
-      finalImageTag = "3.16.0";
+      finalImageTag = "3.18.4";
     };
   };
 }
